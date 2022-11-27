48a347f2d4bcd691f31f3922f23260be064e6b76
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-11-27 06:02:05 -0600

    Make splash page

---
+++ src/pages/Splash.js
@@ -0,0 +1,18 @@
+import Plaque from '../components/plaque';
+
+const SplashPage = () => {
+  return (
+    <section class="splash-page" id="splash-page">
+      <div class="splash-space" />
+      <div class="bg-dark" />
+      <img class="curtain" id="left" src="./images/bg/curtains.jpg" alt="" />
+      <img class="curtain" id="right" src="./images/bg/curtains.jpg" alt="" />
+      <Plaque>
+        <h1>Humaira Zaman</h1>
+        <h3>Software Developer</h3>
+      </Plaque>
+    </section>
+  );
+};
+
+export default SplashPage;
