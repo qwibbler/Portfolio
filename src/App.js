8e3cb6a510905bbaf3ec3654efd2d6d0bde64bf1
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-11-28 17:42:37 -0600

    Add About page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import SplashPage from './pages/Splash';
 import WelcomePage from './pages/Welcome';
 import ProjectPage from './pages/Projects';
+import AboutPage from './pages/About';
 import './App.css';
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <div class="gap" />
         <ProjectPage />
         <div class="gap" />
+        <AboutPage />
       </main>
     </>
   );
