9b762806aa7c897628a8d67858b73f7704c34633
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-11-29 14:01:30 -0600

    Change pic to react

--- src/pages/Splash.js
+++ src/pages/Splash.js
@@ -1,12 +1,13 @@
 import Plaque from '../components/Plaque';
+import Curtain from '../images/bg/curtains.jpg';
 
 const SplashPage = () => {
   return (
     <section className="splash-page" id="splash-page">
       <div className="splash-space" />
       <div className="bg-dark" />
-      <img className="curtain" id="left" src="./images/bg/curtains.jpg" alt="" />
-      <img className="curtain" id="right" src="./images/bg/curtains.jpg" alt="" />
+      <img className="curtain" id="left" src={Curtain} alt="" />
+      <img className="curtain" id="right" src={Curtain} alt="" />
       <Plaque>
         <h1>Humaira Zaman</h1>
         <h3>Software Developer</h3>
