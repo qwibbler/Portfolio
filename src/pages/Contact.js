14cb55678c983002af1f68b7126a2c62537faf21
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-11-29 14:00:44 -0600

    Move Images

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -4,27 +4,27 @@ import FormInput from "../components/FormInput";
 
 const ContactPage = () => (
   <section className="contact-page page" id="contact-me">
-        <Heading>Contact Me</Heading>
-        <InfoBox>
-            I'm always interested in hearing about new projects,
-            so if you'd like to chat please get in touch.
-        </InfoBox>
-        <form className="cards" action="https://formspree.io/f/moqyzrzd" method="post">
-          <div className="card">
-            <FormInput label="Name" id="username">
-              <input type="text" id="username" name="user-name" placeholder="Full Name" maxLength="30" required />
-            </FormInput>
-            <FormInput label="Email" id="useremail">
-              <input type="email" id="useremail" name="user-email" placeholder="Email address" required />
-            </FormInput>
-            <FormInput label="Message" id="comment">
-              <textarea name="msg" id="comment" maxLength="500" placeholder="Message" required></textarea>
-            </FormInput>
-            {/* <input type="text" name="_gotcha" style="display:none" /> */}
-          </div>
-          <div className="btn"><button type="submit">Get In Touch</button></div>
-        </form>
-      </section>
+    <Heading>Contact Me</Heading>
+    <InfoBox>
+        I'm always interested in hearing about new projects,
+        so if you'd like to chat please get in touch.
+    </InfoBox>
+    <form className="cards" action="https://formspree.io/f/moqyzrzd" method="post">
+      <div className="card">
+        <FormInput label="Name" id="username">
+          <input type="text" id="username" name="user-name" placeholder="Full Name" maxLength="30" required />
+        </FormInput>
+        <FormInput label="Email" id="useremail">
+          <input type="email" id="useremail" name="user-email" placeholder="Email address" required />
+        </FormInput>
+        <FormInput label="Message" id="comment">
+          <textarea name="msg" id="comment" maxLength="500" placeholder="Message" required></textarea>
+        </FormInput>
+        {/* <input type="text" name="_gotcha" style="display:none" /> */}
+      </div>
+      <div className="btn"><button type="submit">Get In Touch</button></div>
+    </form>
+  </section>
 );
 
 export default ContactPage;
