c47bef3ab86ea3465fb1f36498f4e467124bd34b
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-11-29 14:06:31 -0600

    Fix PCard variables

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,12 +1,12 @@
 const ProjectCard = ({ project }) => (
   <div className='card-space'>
-    <div className='card-pos-fixed ${orientation}'>
+    <div className='card-pos-fixed {orientation}'>
       <div className='card to-fold'>
         <div className='card-content'>
           <img src='images/project_img/Gmail.png' alt='projimg' />
-          <h4>${project.title}</h4>
-          <p>${project.desc}</p>
-          <ul className='project-list'>${project.list}</ul>
+          <h4>{project.title}</h4>
+          <p>{project.desc}</p>
+          <ul className='project-list'>{project.list}</ul>
         </div>
       </div>
       <div className='btn'>
