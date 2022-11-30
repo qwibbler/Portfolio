1e105f9353eb99a6db5bdd5ef8f03e0add688f16
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-11-30 13:54:59 -0600

    Fix ProjectCard inView

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -1,19 +1,29 @@
-const ProjectCard = ({ project }) => (
-  <div className='card-space'>
-    <div className='card-pos-fixed {orientation}'>
-      <div className='card to-fold'>
-        <div className='card-content'>
-          <img src='images/project_img/Gmail.png' alt='projimg' />
-          <h4>{project.title}</h4>
-          <p>{project.desc}</p>
-          <ul className='project-list'>{project.list}</ul>
-        </div>
-      </div>
-      <div className='btn'>
-        <button>See Project</button>
-      </div>
+import { useInView } from 'react-intersection-observer';
+
+const ProjectCard = ({ index, project }) => {
+  const { ref, inView } = useInView();
+
+  return (
+    <div ref={ref} className="card-space">
+      {inView && (
+        <>
+          <div className={'card-pos-fixed ' + (index % 2)}>
+            <div className="card to-fold">
+              <div className="card-content">
+                <img src="images/project_img/Gmail.png" alt="projimg" />
+                <h4>{project.title}</h4>
+                <p>{project.desc}</p>
+                <ul className="project-list">{project.list}</ul>
+              </div>
+            </div>
+            <div className="btn">
+              <button>See Project</button>
+            </div>
+          </div>
+        </>
+      )}
     </div>
-  </div>
-);
+  );
+};
 
 export default ProjectCard;
