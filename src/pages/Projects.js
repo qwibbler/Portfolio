df83a671b630269521185ed1b315a665556e6606
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-11-30 13:34:40 -0600

    Add projects

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,18 +1,25 @@
 import Heading from '../components/Heading';
 import { useInView } from 'react-intersection-observer';
-import { pageOptions } from '../helper';
+// import { pageOptions } from '../helper';
+import { projectsData } from '../helper';
+import ProjectCard from '../components/ProjectCard';
 
 const ProjectPage = () => {
-  const { ref, inView } = useInView(pageOptions);
+  const { ref, inView } = useInView();
 
   return (
     <section ref={ref} className="project page" id="second-page">
-      {inView && <>
-        <Heading>Featured Works ➢</Heading>
-        <div className="projects cards" id="projects">
-          {/* < className="project_cards" id="popupWrapper" /> */}
-        </div>
-      </>}
+      {inView && (
+        <>
+          <Heading>Featured Works ➢</Heading>
+          <div className="projects cards" id="projects">
+            {projectsData.map((project, index) => (
+              <ProjectCard key={index} project={project} />
+            ))}
+            {/* < className="project_cards" id="popupWrapper" /> */}
+          </div>
+        </>
+      )}
     </section>
   );
 };
