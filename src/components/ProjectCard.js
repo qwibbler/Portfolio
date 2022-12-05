a87118681fae8367283036950caa6efc1c6b1829
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-12-05 07:00:26 -0600

    Move card space outside foldable

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -2,21 +2,19 @@ import img from '../images/project_img/Gmail.png';
 
 const ProjectCard = ({ index, project, onclick }) => {
   return (
-    <div className={'card-space ' + (index % 2)}>
-      {/* <div className="card-pos-fixed"> */}
-        <div className="card to-fold">
-          <div className="card-content">
-            <img src={img} alt="projimg" />
-            <h4>{project.title}</h4>
-            <p>{project.desc}</p>
-            <ul className="project-list">{project.list}</ul>
-          </div>
+    <>
+      <div className={"card to-fold " + (index % 2)}>
+        <div className="card-content">
+          <img src={img} alt="projimg" />
+          <h4>{project.title}</h4>
+          <p>{project.desc}</p>
+          <ul className="project-list">{project.list}</ul>
         </div>
-        <div className="btn">
-          <button onClick={onclick}>See Project</button>
-        </div>
-      {/* </div> */}
-    </div>
+      </div>
+      <div className="btn">
+        <button onClick={onclick}>See Project</button>
+      </div>
+    </>
   );
 };
 
