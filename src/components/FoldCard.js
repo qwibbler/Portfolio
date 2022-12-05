c6d17010dbf2f7b074a0d29ad70e3a891d785933
qwibbler <88380694+qwibbler@users.noreply.github.com>
2022-12-05 04:49:15 -0600

    Check if foldable coming or going

--- src/components/FoldCard.js
+++ src/components/FoldCard.js
@@ -1,9 +1,21 @@
 import Foldable from "./Foldable";
 import ProjectCard from "./ProjectCard";
 import { useState } from "react";
+import { useInView } from "react-intersection-observer";
+import { buildThreshold } from "../helpers/helper";
 
 const FoldCard = ({ index, project }) => {
   const [folding, setFolding] = useState(false);
+  const [ratio, setRatio] = useState(0);
+
+  const { ref } = useInView({
+    threshold: buildThreshold(10),
+    onChange: (inView, entry) => {
+      let coming = ratio < entry.intersectionRatio;
+      setFolding(entry.intersectionRatio > 0.5);
+      setRatio(entry.intersectionRatio);
+    },
+  });
 
   // const finishAction = () => {
   //   setFolding(false);
@@ -13,19 +25,21 @@ const FoldCard = ({ index, project }) => {
     setFolding(true);
     setTimeout(() => {
       setFolding(false);
-    }, 5000);
+    }, 6000);
   }
 
   return (
+    <div ref={ref} className={'card-space'}>
       <Foldable
         isFolded={folding}
-        front={<ProjectCard key={index} index={index} project={project} onclick={onclick} />}
+        front={<ProjectCard key={index} index={index} project={project} onclick={() => {}} />}
         back={<div className="back">Back</div>}
-        duration={2000}
+        duration={4000}
         onCompleteFolding={() => {
           console.log("Folded!");
         }}
       />
+    </div>
   );
 }
 
